962b51ab5ef4ad4ac739364f2720889797dc474b
fujun-iri <fujun-iri@360.cn>
2017-01-12 15:36:26 +0800

    move to browser-sync

--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,32 @@ import resolve from 'rollup-plugin-node-resolve';
 import html from 'rollup-plugin-html';
 import uglify from 'rollup-plugin-uglify';
 import { minify } from 'uglify-js';
-import livereload from 'rollup-plugin-livereload';
-import serve from 'rollup-plugin-serve';
 
 // for css imports
 import { renderSync } from 'node-sass';
 import { createFilter } from 'rollup-pluginutils';
 const cssFilter = createFilter(['**/*.css', '**/*.scss', '**/*.sass']);
+let cssCompressRollupPlugin = {
+  transform(code, id) {
+    if (!cssFilter(id)) {
+      return;
+    }
+
+    let css = renderSync({
+      data: code,
+      outputStyle: 'compressed'
+    }).css.toString();
+
+    return {
+      code: 'export default ' + JSON.stringify(css),
+      map: { mappings: '' }
+    };
+  }
+};
 
 // fix Object.freeze AND keyword `default`
 // see https://github.com/futurist/rollup-plugin-es3/blob/master/index.js
-let es3Fix = {
+let es3FixRollupPlugin = {
   removeHash: {
     'freeze': [/Object.freeze\s*\(\s*([^)]*)\)/g, '$1'],
     // default: "bar" -> "default": "bar"
@@ -23,6 +38,7 @@ let es3Fix = {
     // foo.default -> foo["default"]
     'default_3': [/\.default([\.\s]*)/g, '["__def"]$1']
   },
+  
   transformBundle: function (code) {
     let rmHash = this.removeHash;
     for (var k in rmHash) {
@@ -32,7 +48,39 @@ let es3Fix = {
   }
 };
 
-const config = {
+// browser sync
+// Docs: http://www.browsersync.cn/docs/api/
+let browserSyncRolluPlugin = {
+  client: require('browser-sync').create(),
+  clientState: 0,
+
+  clientInit() {
+    let bs = this.client;
+    bs.init({
+      server: './',
+      open: 'local'
+    });
+    bs.watch('*.html').on('change', bs.reload);
+    
+    this.clientState = 1;
+  },
+
+  clientReload() {
+    this.client.reload(['./dist/bundle.js']);
+  },
+
+  ongenerate() {
+    if (this.clientState === 0) {
+      this.clientInit();
+    } else {
+      this.clientReload();
+    }
+  }
+};
+
+let IS_WATCHING_MODE = Array.from(process.argv).includes('-w');
+
+export default {
   entry: 'js/player.js',
   dest: 'dist/bundle.js',
   format: 'umd',
@@ -48,58 +96,14 @@ const config = {
         minifyJS: true
       }
     }),
-    {
-      transform(code, id) {
-        if (!cssFilter(id)) {
-          return;
-        }
-
-        let css = renderSync({
-          data: code,
-          outputStyle: 'compressed'
-        }).css.toString();
-
-        return {
-          code: 'export default ' + JSON.stringify(css),
-          map: { mappings: '' }
-        };
-      }
-    },
+    cssCompressRollupPlugin,
     resolve({
       main: true,
       browser: true,
       extensions: ['.js']
     }),
     commonjs(),
-    es3Fix
+    es3FixRollupPlugin,
+    IS_WATCHING_MODE ? browserSyncRolluPlugin : uglify({}, minify)
   ]
 };
-
-
-let IS_WATCHING_MODE = Array.from(process.argv).includes('-w');
-let isBrowserOpened = false;
-if (IS_WATCHING_MODE) {
-  config.plugins = config.plugins.concat([
-    serve({
-      contentBase: __dirname,
-      historyApiFallback: true,
-      port: 10001
-    }),
-    livereload({
-      watch: __dirname
-    }),
-    {
-      ongenerate() {
-        console.log('opening http://localhost:10001 in browser');
-        if (!isBrowserOpened) {
-          require('opn')('http://localhost:10001');
-        }
-        isBrowserOpened = true;
-      }
-    }
-  ]);
-} else {
-  config.plugins.push(uglify({}, minify));
-}
-
-export default config;
